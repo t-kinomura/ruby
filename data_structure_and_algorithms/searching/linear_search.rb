b6acc76cbde6ddd0aa4b7e75a5e73d06c9052545
Testu <tetsu99soccer@gmail.com>
2020-04-11 12:45:32 +0900

    Add: linear search

---
+++ data_structure_and_algorithms/searching/linear_search.rb
@@ -0,0 +1,26 @@
+# ruby code for linearly search x in array[]
+# If x is present theh return its location,
+# otherwise return -1
+
+def linear_search(target, array)
+  counter = 0
+    while counter < array.length
+      if array[counter] == target
+        return counter
+      else
+        counter += 1
+      end
+    end
+  return -1
+end
+
+array = [1, 4, 6, 13, 43, 53, 135, 355, 454, 6442]
+target = 13
+index = linear_search(target, array)
+
+puts "Target #{target} index : #{index}"
+
+target = 32
+index = linear_search(target, array)
+
+puts "Target #{target} index : #{index}"
